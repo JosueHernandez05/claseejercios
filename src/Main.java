281705984a8bc54b11557c5c419e81af6332ec02
Josué Hernández <mh24055@ues.edu.sv>
2025-08-26 20:20:30 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,24 @@ public class Main {
     public static void main(String[] args) {
       
         // Crear un objeto de tipo Coche
-
+Coche Coche1 = new coche ("Nissan", "Sentra", "Gris");
 
          // Mostrar información inicial del coche
-
+public void describir() { 
+String estado = this.encendido ? "encendido" : 
+"apagado"; 
+System.out.println("Este es un " + this.marca + " " + 
+this.modelo + " de color " + this.color + " y está " + estado 
++ "."); 
+} 
+}
+    Coche.describir();
         // Encender el coche
-
+Coche1.arrancar();
                 // Cambiar el color del coche
-
+Coche1.pintarCoche("Coche1")
                 // Apagar el coche
-     
+     Coche1.apagar
         }
     }
 }
